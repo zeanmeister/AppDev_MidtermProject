ba90f6cd4bf71b4ac0c942ab1fb25ceaef458803
Gideon <kideonloler@gmail.com>
2025-11-13 05:46:43 +0800

    Updated to a detailed version for debugger_demo.py

--- IntegratedDebugger/debugger_demo.py
+++ IntegratedDebugger/debugger_demo.py
@@ -1,6 +1,37 @@
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
-print("6")
+import random
+from operator import truediv
+
+
+def generateRandom(upper):
+    """
+
+    :param upper:
+    :return: int
+    """
+
+    r = random.randint(1,upper)
+    return r
+
+def main():
+
+    run = True
+    number1 = generateRandom(5)
+    number2 = generateRandom(5)
+    result = number1 + number2
+    while run:
+        ans = input("What is " + str(number1) + " and " + str(number2) + "? ")
+
+        if ans.isdigit():
+            if int(ans) == result:
+                print("Tama!")
+                run = False
+            else:
+                print("Wrong! Try again!!")
+
+        else:
+            print("Answer must be a positive integer!")
+
+times = 10
+
+for x in range(times):
+    main()
